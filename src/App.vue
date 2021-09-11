ecf6ffcccbc2e778d90a60039eca756edaa99ef9
김영욱 <youngwook.kim@mobin-inc.com>
2021-09-11 13:29:52 +0900

    변수 선언 및 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h4>{{ products[0] }}</h4>
+      <p>{{ price1 }} 만원</p>
+    </div>
+    <div>
+      <h4>{{ products[1] }}</h4>
+      <p>{{ price2 }} 만원</p>
+    </div>
+    <div>
+      <h4>{{ products[2] }}</h4>
+      <p>{{ price2 }} 만원</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      price1: 60,
+      price2: 70,
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  components: {}
 }
 </script>
 
