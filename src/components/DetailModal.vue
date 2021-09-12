edcf921044a7e32cffc669883ebc282d6301abee
김영욱 <youngwook.kim@mobin-inc.com>
2021-09-12 16:44:18 +0900

    input 데이터 입력감시 watcher

--- src/components/DetailModal.vue
+++ src/components/DetailModal.vue
@@ -5,9 +5,9 @@
             <h4>{{ products[viewIdx].title }}</h4>
             <p>{{ products[viewIdx].content }}</p>
             <!--<input @input="month = $event.target.value">-->
-            <input v-model.number="month">
+            <input v-model="month">
             <p> {{ month }}개월 선택함 : {{ month * products[viewIdx].price }} 원</p>
-            <button @click="$emit('closeDetail')">닫기</button>
+            <button @click="closeDetail">닫기</button>
         </div>
     </div>
 </template>
@@ -20,10 +20,28 @@ export default {
             month: 1
         }
     },
+    watch: {
+        month(v, before) {
+            this.month = v.trim();
+            if(isNaN(v)) {
+                alert('숫자만 입력 가능합니다.')
+                this.month = before;
+            } else if(v >= 13 || v <= 0) {
+                alert('1~12사이의 숫자만 입력 가능합니다.')
+                this.month = before;
+            }
+        }
+    },
     props: {
         products: Array,
         detailIsOpened: Boolean,
         viewIdx: Number
+    },
+    methods: {
+        closeDetail() {
+            this.$emit('closeDetail');
+            this.month = 1;
+        }
     }
 }
 </script>
